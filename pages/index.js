3af80d8f01ba4fce7ee40ccca359a19b51986852
Myles O'Connor <myles@oconnor.industries>
2021-04-28 17:55:33 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,23 +13,22 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">ioTree</a>
+          Welcome to <a href="https://iotree.org">ioTree</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Purchase one or many trees and own their unique NFTs
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find in-depth information about ioTree features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://iotree.org" className={styles.card}>
             <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Learn about ioTree in an interactive course with quizzes!</p>
           </a>
 
           <a
@@ -37,16 +36,16 @@ export default function Home() {
             className={styles.card}
           >
             <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <p>Browse the trees.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Purchase a tree</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Let's get started.
             </p>
           </a>
         </div>
