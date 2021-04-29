71d1b384b38a751c5bf8e22707ad33dde07f1039
Myles O'Connor <myles@oconnor.industries>
2021-04-28 17:43:15 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const currentYear = new Date().getFullYear()
+  
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Welcome to ioTree</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">ioTree</a>
         </h1>
 
         <p className={styles.description}>
@@ -51,14 +53,9 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>
+          &copy; {currentYear} ioTree
+        </p>
       </footer>
     </div>
   )
