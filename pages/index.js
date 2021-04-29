09ce9dc65d3fa4fd79c07ad6d0f511fe9dbdc0e9
Myles O'Connor <myles@oconnor.industries>
2021-04-28 18:02:31 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Purchase one or many trees and own their unique NFTs
+          Purchase one or many trees, and prove your unique ownership with NFTs.
         </p>
 
         <div className={styles.grid}>
